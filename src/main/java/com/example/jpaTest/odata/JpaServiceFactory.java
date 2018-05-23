96f80f9e9734fbadef53137f312630ac43fdcdf9
Zoltan Hodgyai <zoltan.hodgyai@msg.group>
2018-05-23 13:30:58 +0300

    Edm Extension

--- src/main/java/com/example/jpaTest/odata/JpaServiceFactory.java
+++ src/main/java/com/example/jpaTest/odata/JpaServiceFactory.java
@@ -1,13 +1,25 @@
 package com.example.jpaTest.odata;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.olingo.odata2.api.ODataCallback;
+import org.apache.olingo.odata2.api.ODataDebugCallback;
+import org.apache.olingo.odata2.api.commons.HttpStatusCodes;
+import org.apache.olingo.odata2.api.ep.EntityProvider;
+import org.apache.olingo.odata2.api.exception.ODataApplicationException;
+import org.apache.olingo.odata2.api.processor.ODataErrorCallback;
+import org.apache.olingo.odata2.api.processor.ODataErrorContext;
+import org.apache.olingo.odata2.api.processor.ODataResponse;
 import org.apache.olingo.odata2.jpa.processor.api.ODataJPAContext;
 import org.apache.olingo.odata2.jpa.processor.api.ODataJPAServiceFactory;
 import org.apache.olingo.odata2.jpa.processor.api.exception.ODataJPARuntimeException;
+import org.apache.olingo.odata2.jpa.processor.api.model.JPAEdmExtension;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManagerFactory;
+import javax.validation.constraints.Null;
 
 @Component
+@Slf4j
 public class JpaServiceFactory extends ODataJPAServiceFactory {
 
     private static final String PERSISTENCE_UNIT_NAME = "jpaTest";
@@ -24,10 +36,40 @@ public class JpaServiceFactory extends ODataJPAServiceFactory {
         try {
             oDataJPAContext.setEntityManagerFactory(entityManagerFactory);
             oDataJPAContext.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
-//            setJPAEdmExtension
+            oDataJPAContext.setJPAEdmExtension(new JpaExtension());
             return oDataJPAContext;
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ODataCallback> T getCallback(Class<T> callbackInterface) {
+
+        return (T) (callbackInterface.isAssignableFrom(ScenarioErrorCallback.class) ? new ScenarioErrorCallback()
+                : callbackInterface.isAssignableFrom(ODataDebugCallback.class) ? new ScenarioDebugCallback()
+                : super.getCallback(callbackInterface));
+
+    }
+
+    private final class ScenarioDebugCallback implements ODataDebugCallback {
+
+        @Override
+        public boolean isDebugEnabled() {
+            return true;
+        }
+    }
+
+    private class ScenarioErrorCallback implements ODataErrorCallback {
+
+        @Override
+        public ODataResponse handleError(final ODataErrorContext context) throws ODataApplicationException {
+            if (context.getHttpStatus() == HttpStatusCodes.INTERNAL_SERVER_ERROR) {
+                log.error("Internal Server Error: " + context.getException().toString());
+            }
+
+            return EntityProvider.writeErrorDocument(context);
         }
     }
 }
